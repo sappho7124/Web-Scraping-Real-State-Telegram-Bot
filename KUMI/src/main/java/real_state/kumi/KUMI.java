23e574b788205b39bb4250e9533110e6dee89a2d
Santiago Mejia W <santiago.mejia.7124@gmail.com>
2023-05-11 16:46:13 -0500

    Changed to a maven project and added the dependencies to the pom.xml file
    moved the web scraper to its own folder but the main python executable is still in the root folder
    
    added the java files to the project
    
    doesnt work as of now but the project is now a maven project

---
+++ KUMI/src/main/java/real_state/kumi/KUMI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package real_state.kumi;
+
+/**
+ *
+ * @author santi
+ */
+public class KUMI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
